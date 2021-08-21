db287b058df33c623a9415d8e657e25fab18310e
javin1102 <javinrio112@gmail.com>
2021-08-21 11:38:16 +0700

    test deploy2

---
+++ .next/static/HjowgtV1vmAzKni75jBkO/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/61-63d7fdbc1c742c730a23.js","static/chunks/pages/index-740ecd06513bc4422ddb.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
